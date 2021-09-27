0a5d98996f8d94d64d5e586f189c6eae9432bc1b
belijzajac <tautvydas749@gmail.com>
2021-09-27 20:14:48 +0300

    Added prebuilt ckzg and blst libs and their header files Linked against ckzg lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#[repr(C)]
+pub enum KzgRet {
+    KzgOk = 0,
+    KzgBadArgs,
+    KzgError,
+    KzgMalloc
+}
+
+#[link(name = "ckzg", kind = "static")]
+extern "C" {
+    fn hello() -> KzgRet;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
