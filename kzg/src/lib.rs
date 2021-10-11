bf9bce25f7ac87e8bca6a1dcf6335dd6db492853
Stokalas <gytis.becalis@gmail.com>
2021-10-11 15:33:39 +0300

    other files

--- kzg/src/lib.rs
+++ kzg/src/lib.rs
@@ -1,15 +1,4 @@
-pub type Pairing = blst::Pairing;
-pub type Fp = blst::blst_fp;
-pub type Fp12 = blst::blst_fp12;
-pub type Fp6 = blst::blst_fp6;
-pub type Fr = blst::blst_fr;
-pub type P1 = blst::blst_p1;
-pub type P1Affine = blst::blst_p1_affine;
-pub type P2 = blst::blst_p2;
-pub type P2Affine = blst::blst_p2_affine;
-pub type Scalar = blst::blst_scalar;
-pub type Uniq = blst::blst_uniq;
 
-pub mod finite;
+pub mod implem;
 
 
