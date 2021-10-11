c31675c17822160b21ebdc0d0df9014db8d40be6
Stokalas <gytis.becalis@gmail.com>
2021-10-11 15:30:41 +0300

    restructured project

--- kzg-bench/benches/finite.rs
+++
@@ -1,8 +0,0 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("add_fr", |b| b.iter(|| kzg::finite::add_fr(black_box(kzg::Fr::default()), black_box(kzg::Fr::default()))));
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
--- kzg-bench/src/lib.rs
+++ kzg-bench/src/lib.rs
@@ -1,2 +1,1 @@
-mod finite;
-mod poly;
+mod test;
