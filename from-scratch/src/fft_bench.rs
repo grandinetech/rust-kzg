d1de8a59f6fa1bd310f31e304040c2483d81ded4
Mantulis00 <mantas.radzevicius1@gmail.com>
2021-10-13 00:52:20 +0300

    Add files via upload

---
+++ from-scratch/src/fft_bench.rs
@@ -0,0 +1,37 @@
+use kzg::Fr;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use crate::kzg_types::{create_fr_rand, FFTSettings};
+fn bench_fft_fr(c: &mut Criterion) {
+    for scale in 4..16 {
+        _fft_fr(scale, c);
+    }
+}
+
+fn _fft_fr(scale: u8, c: &mut Criterion) {
+    let settings = FFTSettings::new(scale);
+    let mut data: Vec<Fr> = (0..(settings.max_width >> 1)).map(|_| create_fr_rand()).collect();
+    let id = format!("bench_fft_fr scale: '{}'", scale);
+    c.bench_function(&id, |b| b.iter(|| settings.fft(&mut data, false)));
+}
+
+/*fn bench_fft_g1(c: &mut Criterion) {
+    assert!(init(CurveType::BLS12_381));
+    for scale in 4..16 {
+        _fft_g1(scale, c);
+    }
+}
+
+fn _fft_g1(scale: u8, c: &mut Criterion) {
+    let settings = FFTSettings::new(scale);
+    let curve = Curve::new(&Fr::random(), 2);
+
+    let data: Vec<G1> = (0..(settings.max_width >> 1)).map(|_| &curve.g1_gen * &Fr::random()).collect();
+    let id = format!("bench_fft_g1 scale: '{}'", scale);
+    c.bench_function(&id, |b| b.iter(|| settings.fft_g1(&data)));
+}
+*/
+
+
+//criterion_group!(benches, bench_fft_fr, bench_fft_g1);
+criterion_group!(benches, bench_fft_fr);
+criterion_main!(benches);
