72802e41f008c3b2e76f3155fa894b378b0a19bc
belijzajac <tautvydas749@gmail.com>
2021-10-14 20:32:29 +0300

    Added function to make Vec<G1> data (based on blst's g1_add_or_dbl function) Exposed user-friendly api for fft_g1

--- kzg/src/fftsettings.rs
+++ kzg/src/fftsettings.rs
@@ -37,12 +37,45 @@ pub struct BlstP1 {
     pub z: BlstFp
 }
 
+const G1_GENERATOR: G1 = G1 {
+    x: BlstFp {
+        l: [0x5cb38790fd530c16,
+            0x7817fc679976fff5,
+            0x154f95c7143ba1c1,
+            0xf0ae6acdf3d0e747,
+            0xedce6ecc21dbf440,
+            0x120177419e0bfb75
+        ]
+    },
+    y: BlstFp {
+        l: [0xbaac93d50ce72271,
+            0x8c22631a7918fd8e,
+            0xdd595f13570725ce,
+            0x51ac582950405194,
+            0x0e1c8c3fad0059c0,
+            0x0bbc3efc5008a26a
+        ]
+    },
+    z: BlstFp {
+        l: [0x760900000002fffd,
+            0xebf4000bc40c0002,
+            0x5f48985753c758ba,
+            0x77ce585370525745,
+            0x5c071a97a256ec6d,
+            0x15f65ec3fa80e493
+        ]
+    }
+};
+
 extern "C" {
     fn new_fft_settings(settings: *mut FFTSettings, max_scale: u32) -> Error;
     fn free_fft_settings(settings: *mut FFTSettings);
     fn fft_fr(output: *mut Fr, input: *const Fr, inverse: bool, n: u64, fs: *const FFTSettings) -> Error;
-    fn fft_g1(output: *mut G1, input: *mut G1, inverse: bool, n: u64, fs: *const FFTSettings) -> Error;
+    fn fft_g1(output: *mut G1, input: *const G1, inverse: bool, n: u64, fs: *const FFTSettings) -> Error;
     fn poly_mul(output: *mut Poly, a: *const Poly, b: *const Poly, fs: *const FFTSettings) -> Error;
+    // Blst
+    fn g1_add_or_dbl(out: *mut G1, a: *const G1, b: *const G1);
+    fn g1_equal(a: *const G1, b: *const G1) -> bool;
 }
 
 impl FFTSettings {
@@ -77,7 +110,7 @@ impl FFTSettings {
     pub fn fft_fr(&self, input: &mut Vec<Fr>, inverse: bool) -> Vec<Fr> {
         let output = match FFTSettings::_fft_fr(input.as_mut_ptr(), inverse, input.len() as u64, self) {
             Ok(fr) => fr,
-            Err(e) => panic!("Error in \"FFTSettings::fft\" ==> {:?}", e)
+            Err(e) => panic!("Error in \"FFTSettings::fft_fr\" ==> {:?}", e)
         };
         output
     }
@@ -87,24 +120,63 @@ impl FFTSettings {
         unsafe {
             return match fft_fr(output.as_mut_ptr(), input, inverse, n, fs) {
                 Error::KzgOk => Ok(output),
-                e => {
-                    println!("Error in \"FFTSettings::fft_fr\" ==> {:?}", e);
-                    Err(e)
-                }
+                e => Err(e)
             }
         }
     }
 
-    pub fn fft_g1(input: *mut G1, inverse: bool, n: u64, fs: *const FFTSettings) -> Result<G1, Error> {
-        let mut output = G1::default();
+    pub fn fft_g1(&self, input: &mut Vec<G1>, inverse: bool) -> Vec<G1> {
+        let output = match FFTSettings::_fft_g1(input.as_mut_ptr(), inverse, input.len() as u64, self) {
+            Ok(g) => g,
+            Err(e) => panic!("Error in \"FFTSettings::fft_g1\" ==> {:?}", e)
+        };
+        output
+    }
+
+    fn _fft_g1(input: *mut G1, inverse: bool, n: u64, fs: *const FFTSettings) -> Result<Vec<G1>, Error> {
+        let mut output = vec![G1::default(); n as usize];
         unsafe {
-            return match fft_g1(&mut output, input, inverse, n, fs) {
+            return match fft_g1(output.as_mut_ptr(), input, inverse, n, fs) {
                 Error::KzgOk => Ok(output),
-                e => {
-                    println!("Error in \"FFTSettings::fft_g1\" ==> {:?}", e);
-                    Err(e)
-                }
+                e => Err(e)
             }
+        }
+    }
+
+    pub fn make_data(n: usize) -> Vec<G1> {
+        // It's not mentioned in c-kzg implementation but for safety reasons allocate (n + 1) space
+        // because we'll try to access one element further its initial size in the first for loop
+        let mut out_val = vec![G1_GENERATOR; n + 1];
+        let out_ptr: *mut G1 = out_val.as_mut_ptr();
+        // Multiples of g1_gen
+        if n == 0 { return vec![G1::default(); 0]; }
+        for i in 1..n as isize {
+            unsafe {
+                *out_ptr.offset(i + 1) = FFTSettings::add_or_dbl(*out_ptr.offset(i - 1), &G1_GENERATOR);
+            }
+        }
+        unsafe {
+            let mut out = vec![G1::default(); 0];
+            let mut i: isize = 0;
+            while !out_ptr.offset(i).is_null() && i < n as isize {
+                out.push(*out_ptr.offset(i) as G1);
+                i += 1;
+            }
+            return out
+        }
+    }
+
+    fn add_or_dbl(a: G1, b: *const G1) -> G1 {
+        let mut out = G1::default();
+        unsafe {
+            g1_add_or_dbl(&mut out, &a, b);
+        }
+        out
+    }
+
+    pub fn g1_equal(a: *const G1, b: *const G1) -> bool {
+        unsafe {
+            return g1_equal(a, b);
         }
     }
 
