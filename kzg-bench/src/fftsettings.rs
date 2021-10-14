fce01d2c1a8eac32285ca7ac1ae7e68329da62a9
belijzajac <tautvydas749@gmail.com>
2021-10-14 20:42:36 +0300

    Added test for FFT over the finite field (G1)

--- kzg-bench/src/fftsettings.rs
+++ kzg-bench/src/fftsettings.rs
@@ -13,7 +13,7 @@ mod tests {
     }
 
     #[test]
-    fn roundtrip_fft() {
+    fn roundtrip_fft_fr() {
         let size: u32 = 12;
         let mut fs = match FFTSettings::new(size) {
             Ok(s) => s,
@@ -32,6 +32,29 @@ mod tests {
         for i in 0..fs.max_width {
             let temp = Fr::from_u64(i as u64);
             assert_eq!(Fr::is_equal(temp, data[i]), true);
+        }
+        fs.destroy();
+    }
+
+    #[test]
+    fn roundtrip_fft_g1() {
+        let size: u32 = 10;
+        let mut fs = match FFTSettings::new(size) {
+            Ok(s) => s,
+            Err(_) => FFTSettings::default()
+        };
+        assert_eq!(fs.max_width, 2 << size - 1);
+        // make_data
+        let expected = FFTSettings::make_data(fs.max_width);
+        let mut data = FFTSettings::make_data(fs.max_width);
+        // Forward and reverse FFT
+        let mut coeffs = fs.fft_g1(&mut data, false);
+        assert_eq!(coeffs.len(), 2 << size - 1);
+        data = fs.fft_g1(&mut coeffs, true);
+        assert_eq!(data.len(), 2 << size - 1);
+        // Verify that the result is still ascending values of i
+        for i in 0..fs.max_width {
+            assert_eq!(FFTSettings::g1_equal(&expected[i], &data[i]), true);
         }
         fs.destroy();
     }
