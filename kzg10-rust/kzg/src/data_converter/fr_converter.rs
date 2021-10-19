f8a73e8481c3713456c09cee27a2640c4a72c7f4
Yar3k <djelement567@gmail.com>
2021-10-19 22:34:56 +0300

    Refactor

--- kzg10-rust/kzg/src/data_converter/fr_converter.rs
+++ kzg10-rust/kzg/src/data_converter/fr_converter.rs
@@ -2,11 +2,19 @@ use crate::data_types::fr::Fr;
 use crate::BlstFr;
 
 pub fn frFromBlst(fr: BlstFr) -> Fr {
-    let mut result = Fr::default();
-    for i in 0..4 {
-        result.d[i] = fr.l[i];
+    let mut intValue: u64 = 0;
+    let frBlst = fr;
+    unsafe {
+        blst::blst_uint64_from_fr(&mut intValue, &frBlst);
     }
-    return result;
+    let i32Value = intValue as i32;//could cause problems
+    return Fr::from_int(i32Value);
+    
+    // let mut result = Fr::default();
+    // for i in 0..4 {
+    //     result.d[i] = fr.l[i];
+    // }
+    // return result;
 }
 
 pub fn frToBlst(fr: Fr) -> BlstFr {
