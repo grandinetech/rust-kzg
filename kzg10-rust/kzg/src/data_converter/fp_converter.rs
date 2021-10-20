5312c4723f059a6de8eb946b46bcdc2c84cdcc42
Stokalas <gytis.becalis@gmail.com>
2021-10-20 13:46:33 +0300

    fp converter fix

--- kzg10-rust/kzg/src/data_converter/fp_converter.rs
+++ kzg10-rust/kzg/src/data_converter/fp_converter.rs
@@ -2,6 +2,15 @@ use crate::data_types::fp::Fp;
 use crate::BlstFp;
 
 pub fn fp_from_blst(fp: BlstFp) -> Fp {
+    let mut intValue: u64 = 0;
+    let fpblst = fp;
+    unsafe{
+        blst::blst_uint64_from_fp(&mut intValue, &fpblst)
+    }
+    let i32Value = intValue as i32;
+    return Fp::from_int(i32Value);
+    
+    /*
     let mut result = Fp::default();
     for i in 0..4 {
         result.d[i] = fp.l[i];
