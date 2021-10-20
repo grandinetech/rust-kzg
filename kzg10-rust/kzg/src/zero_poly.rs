c14ce0971dcaa62f00f2543283ec980c1da5ec40
ciliakas <ciliakas@gmail.com>
2021-10-20 14:43:50 +0300

    fixed zero poly value management

--- kzg10-rust/kzg/src/zero_poly.rs
+++ kzg10-rust/kzg/src/zero_poly.rs
@@ -1,24 +1,26 @@
-use std::{cmp::min};
-use crate::old::*;
-use crate::utilities::next_pow_of_2;
-use crate::kzg10::Polynomial;
-use crate::data_types::{fr::*};
+use crate::data_types::fr::*;
 use crate::fk20_fft::FFTSettings;
-
+use crate::kzg10::Polynomial;
+use crate::utilities::next_pow_of_2;
+use std::cmp::min;
 
 impl FFTSettings {
     // TODO: could be optimized by using mutable slices!
-    pub fn zero_poly_via_multiplication_base(&self, indices: &[usize], length: usize, optimized: bool) -> (Vec<Fr>, Vec<Fr>) {
+    pub fn zero_poly_via_multiplication(
+        &self,
+        indices: &[usize],
+        length: usize,
+    ) -> (Vec<Fr>, Vec<Fr>) {
         if indices.is_empty() {
             return (vec![Fr::zero(); length], vec![Fr::zero(); length]);
         }
-        
+        let stride = self.max_width / length;
         let per_leaf_poly = 64;
         let per_leaf = per_leaf_poly - 1;
-        //TO DO fix the optimisation, possibly using unsafe static memory block
-        if indices.len() <= per_leaf || !optimized {
+        
+        if indices.len() <= per_leaf {
             let mut zero_poly = vec![Fr::default(); length];
-            self.make_zero_poly_mul_leaf(&mut zero_poly, indices);
+            self.make_zero_poly_mul_leaf(&mut zero_poly, indices, stride);
 
             let zero_eval = self.fft(&zero_poly, false);
             return (zero_eval, zero_poly);
@@ -27,19 +29,26 @@ impl FFTSettings {
         let leaf_count = (indices.len() + per_leaf - 1) / per_leaf;
         let n = next_pow_of_2(leaf_count * per_leaf_poly);
 
-        // TODO: rust limitation, can't have multiple mutators for same value, code fails somewhere here, as I tried to achieve same func through duplicated value management.
+
+        //TO DO improve optimisation, possibly using unsafe static memory block
         let mut out = vec![Fr::default(); n];
         let mut offset = 0;
+        let mut out_offset = 0;
         let mut leaves: Vec<Vec<Fr>> = vec![vec![]; leaf_count];
         let max = indices.len();
-        for _ in 0..leaf_count {
+        for i in 0..leaf_count {
             let end = min(offset + per_leaf, max);
-            let mut slice = vec![Fr::default(); per_leaf_poly];
-            self.make_zero_poly_mul_leaf(&mut slice, &indices[offset..end]);
-            let mut slice_copy = slice.clone();
+            leaves[i] = out[out_offset..out_offset + per_leaf_poly].to_vec();
+            self.make_zero_poly_mul_leaf(&mut leaves[i], &indices[offset..end], stride);
+            let mut slice_copy = leaves[i].clone();
             out.append(&mut slice_copy);
-            leaves.push(slice);
             offset += per_leaf;
+            out_offset += per_leaf_poly;
+        }
+
+        out = out[n..].to_vec();
+        for _ in out.len()..n {
+            out.push(Fr::zero());
         }
 
         let reduction_factor = 4;
@@ -53,8 +62,8 @@ impl FFTSettings {
                 let mut end = start + reduction_factor;
 
                 let out_end = min(out.len(), end * leaf_size);
-                let reduced = &mut out[start * leaf_size .. out_end].to_vec();
-                end = min(end, leaves.len()); 
+                let reduced = &mut out[start * leaf_size..out_end].to_vec();
+                end = min(end, leaves.len());
 
                 let leaves_slice = &mut leaves[start..end];
                 if end > start + 1 {
@@ -69,19 +78,17 @@ impl FFTSettings {
 
         return (zero_eval, zero_poly);
     }
-    
-    pub fn zero_poly_via_multiplication(&self, indices: &[usize], length: usize) -> (Vec<Fr>, Vec<Fr>){
-        return self.zero_poly_via_multiplication_base(indices, length, false);
-    }
 
     pub fn reduce_leaves(&self, scratch: &mut [Fr], ps: &[Vec<Fr>], n: usize) -> Vec<Fr> {
-        let out_degree: usize = ps.iter()
+        let out_degree: usize = ps
+            .iter()
             .map(|x| {
-                if x.len() == 0 { 
-                    return 0; 
-                } 
+                if x.len() == 0 {
+                    return 0;
+                }
                 return x.len() - 1;
-            }).sum();
+            })
+            .sum();
         let (p_padded, rest) = scratch.split_at_mut(n);
         let (mul_eval_ps, p_eval) = rest.split_at_mut(n);
 
@@ -118,16 +125,15 @@ impl FFTSettings {
         let result = self.inplace_fft(&mul_eval_ps, true);
         return result[..out_degree + 1].to_vec();
     }
-    
-    pub fn make_zero_poly_mul_leaf(&self, dest: &mut Vec<Fr>, indices: &[usize]) {
+
+    pub fn make_zero_poly_mul_leaf(&self, dest: &mut Vec<Fr>, indices: &[usize], stride: usize) {
         if (indices.len() + 1) > dest.len() {
             panic!("expected bigger dest length");
         }
 
         dest[indices.len()] = Fr::one();
-        
         for (i, v) in indices.iter().enumerate() {
-            let neg_di = self.exp_roots_of_unity[v * 1].get_neg();
+            let neg_di = self.exp_roots_of_unity[v * stride].get_neg();
             dest[i] = neg_di.clone();
             if i > 0 {
                 let temp = &dest[i] + &dest[i - 1];
