b81e96c1d3815307d37672e5e5d5f39773c579a4
Julius-Syvis <55212122+Julius-Syvis@users.noreply.github.com>
2021-10-25 19:14:40 +0300

    comment out non-compiling benches

--- from-scratch/benches/fft.rs
+++ from-scratch/benches/fft.rs
@@ -3,19 +3,19 @@ use kzg::Fr;
 use kzg_from_scratch::fft_fr::fft_fr;
 use kzg_from_scratch::kzg_types::{create_fr_rand, FsFFTSettings};
 
-fn bench_fft_fr(c: &mut Criterion) {
-    for scale in 4..16 {
-        _fft_fr(scale, c);
-    }
-}
-
-fn _fft_fr(scale: u8, c: &mut Criterion) {
-    let fft_settings = FsFFTSettings::from_scale(scale as usize).unwrap();
-    let mut data: Vec<Fr> = vec![create_fr_rand(); fft_settings.max_width];
-    let mut cache: Vec<Fr> = vec![Fr::default(); fft_settings.max_width];
-    let id = format!("bench_fft_fr scale: '{}'", scale);
-    c.bench_function(&id, |b| b.iter(|| fft_fr(&mut cache, &data, false, &fft_settings)));
-}
+// fn bench_fft_fr(c: &mut Criterion) {
+//     for scale in 4..16 {
+//         _fft_fr(scale, c);
+//     }
+// }
+//
+// fn _fft_fr(scale: u8, c: &mut Criterion) {
+//     let fft_settings = FsFFTSettings::from_scale(scale as usize).unwrap();
+//     let mut data: Vec<Fr> = vec![create_fr_rand(); fft_settings.max_width];
+//     let mut cache: Vec<Fr> = vec![Fr::default(); fft_settings.max_width];
+//     let id = format!("bench_fft_fr scale: '{}'", scale);
+//     c.bench_function(&id, |b| b.iter(|| fft_fr(&mut cache, &data, false, &fft_settings)));
+// }
 
 /*fn bench_fft_g1(c: &mut Criterion) {
     assert!(init(CurveType::BLS12_381));
@@ -35,5 +35,5 @@ fn _fft_g1(scale: u8, c: &mut Criterion) {
 */
 
 //criterion_group!(benches, bench_fft_fr, bench_fft_g1);
-criterion_group!(benches, bench_fft_fr);
-criterion_main!(benches);
+// criterion_group!(benches, bench_fft_fr);
+// criterion_main!(benches);
