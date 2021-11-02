91b663515a4ffdcd546ef06f3c85f0e4b2ac708b
Yar3k <djelement567@gmail.com>
2021-11-03 00:22:18 +0200

    Final bench

--- kzg-bench/src/benches/kzg.rs
+++ kzg-bench/src/benches/kzg.rs
@@ -1,33 +1,36 @@
 use criterion::Criterion;
-use kzg::{FFTFr, FFTG1, FFTSettings, Fr, G1};
+use kzg::{FFTSettings, Fr, KZGSettings, Poly, G1, G2};
 
-pub fn bench_kzg<TFr: Fr, TFFTSettings: FFTSettings<TFr> + FFTFr<TFr>>(c: &mut Criterion) {
-    for scale in 1..15 {
-        let mut fft_settings = TFFTSettings::new(scale as usize).unwrap();
-        //Should exist kzg settings
-        //let mut kzg_settings;
+pub const SECRET: [u8; 32usize] = [0xa4, 0x73, 0x31, 0x95, 0x28, 0xc8, 0xb6, 0xea, 0x4d, 0x08, 0xcc,
+                                0x53, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 
-        //Start of Trusted setup
+pub fn kzg_proof<
+        TFr: Fr,
+        TG1: G1,
+        TG2: G2,
+        TPoly: Poly<TFr>,
+        TFFTSettings: FFTSettings<TFr>,
+        TKZGSettings: KZGSettings<TFr, TG1, TG2, TFFTSettings, TPoly>,
+>(
+        generate_trusted_setup: &dyn Fn(usize, [u8; 32usize]) -> (Vec<TG1>, Vec<TG2>),
+) {     
+        for scale in 1..15 {
+        let mut fs = TFFTSettings::new(scale as usize).unwrap();
 
-        //Need to find in mlc fr_from_scale method
-        //let s = Fr::from_scale()
-        //fr_from_scalar(s, secret);
-        let s_pow = TFr::one();
+        let (s1, s2) = generate_trusted_setup(fs.get_max_width(), SECRET);
+        let mut ks = TKZGSettings::new(&s1, &s2, secrets_len, fs.get_max_width());
 
-        for i in 0..fft_settings.get_max_width() {
-            mclBnG1_mul(s1 + i, g1_generator, s_pow);//Not universal solution
-            mclBnG2_mul(s2 + i, g2_generator, s_pow);//Not universal solution
-            mclBnFr_mul(s_pow, s_pow, s);//Not universal solution (Fr::mul exist)
-        }
-        //End of Trusted setup
 
         let mut poly = TPoly::new(fft_settings.get_max_width()).unwrap();
         for i in 0..fft_settings.get_max_width() {
             p.set_coeff_at(i, Fr::rand());
         }
-        let id = format!("bench_kzg scale: '{}'", scale);
-        //Commiting to poly not exist
-        //c.bench_function(&id, |b| b.iter(|| commit_to_poly(&commitment, poly, kzg_settings)));
-        fft_settings.destroy();
+        let id = format!("bench_kzg_proof scale: '{}'", scale);
+        c.bench_function(&id, |b| b.iter(|| ks.commit_to_poly(&poly).unwrap()));
+
+        fs.destroy();
+        ks.destroy();
+        poly.destroy();
     }
 }
