285bf150680f204a136adeae4cdfe1d8fd705a40
Yar3k <djelement567@gmail.com>
2021-11-02 15:27:10 +0200

    Update kzg bench

--- kzg-bench/src/benches/kzg.rs
+++ kzg-bench/src/benches/kzg.rs
@@ -7,15 +7,26 @@ pub fn bench_kzg<TFr: Fr, TFFTSettings: FFTSettings<TFr> + FFTFr<TFr>>(c: &mut C
         //Should exist kzg settings
         //let mut kzg_settings;
 
-        //Copied some kind of method from C
-        //generate_trusted_setup(s1, s2, &secret, fs.max_width);
+        //Start of Trusted setup
+
+        //Need to find in mlc fr_from_scale method
+        //let s = Fr::from_scale()
+        //fr_from_scalar(s, secret);
+        let s_pow = TFr::one();
+
+        for i in 0..fft_settings.get_max_width() {
+            mclBnG1_mul(s1 + i, g1_generator, s_pow);//Not universal solution
+            mclBnG2_mul(s2 + i, g2_generator, s_pow);//Not universal solution
+            mclBnFr_mul(s_pow, s_pow, s);//Not universal solution (Fr::mul exist)
+        }
+        //End of Trusted setup
 
         let mut poly = TPoly::new(fft_settings.get_max_width()).unwrap();
         for i in 0..fft_settings.get_max_width() {
             p.set_coeff_at(i, Fr::rand());
         }
         let id = format!("bench_kzg scale: '{}'", scale);
-        //Someting with commiting poly
+        //Commiting to poly not exist
         //c.bench_function(&id, |b| b.iter(|| commit_to_poly(&commitment, poly, kzg_settings)));
         fft_settings.destroy();
     }
