b78651e29e6e2d1249a1f5f4382e71c09e44425f
belijzajac <tautvydas749@gmail.com>
2021-11-02 19:31:15 +0200

    Cover two new Poly tests

--- ckzg/tests/poly.rs
+++ ckzg/tests/poly.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod tests {
     use kzg_bench::tests::poly::{create_poly_of_length_ten, poly_eval_0_check, poly_eval_check,
-                                 poly_eval_nil_check, poly_inverse_simple_0, poly_inverse_simple_1};
+                                 poly_eval_nil_check, poly_inverse_simple_0, poly_inverse_simple_1,
+                                 poly_test_div, poly_div_by_zero};
     use kzg_bindings::finite::BlstFr;
     use kzg_bindings::poly::KzgPoly;
 
@@ -33,5 +34,15 @@ mod tests {
     #[test]
     fn test_poly_inverse_simple_1() {
         poly_inverse_simple_1::<BlstFr, KzgPoly>();
+    }
+
+    #[test]
+    fn test_poly_test_div() {
+        poly_test_div::<BlstFr, KzgPoly>();
+    }
+
+    #[test]
+    fn test_poly_div_by_zero() {
+        poly_div_by_zero::<BlstFr, KzgPoly>();
     }
 }
