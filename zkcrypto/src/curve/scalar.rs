c4fcf694ca1830941c5bf08a2159fb2520a33ac1
dominykasKaminskas <91206504+dominykasKaminskas@users.noreply.github.com>
2021-11-03 12:48:08 +0200

    Add reduce and divn methods to Scalar

--- zkcrypto/src/curve/scalar.rs
+++ zkcrypto/src/curve/scalar.rs
@@ -829,6 +829,39 @@ impl Scalar {
 
         Scalar([d0 & mask, d1 & mask, d2 & mask, d3 & mask])
     }
+    
+    /// Reduces the scalar and returns it multiplied by the montgomery
+    /// radix.
+    pub fn reduce(&self) -> Scalar {
+        Scalar::montgomery_reduce(self.0[0], self.0[1], self.0[2], self.0[3], 0, 0, 0, 0)
+    }
+
+    /// SHR impl
+    #[inline]
+    pub fn divn(&mut self, mut n: u32) {
+        if n >= 256 {
+            *self = Self::from(0);
+            return;
+        }
+
+        while n >= 64 {
+            let mut t = 0;
+            for i in self.0.iter_mut().rev() {
+                core::mem::swap(&mut t, i);
+            }
+            n -= 64;
+        }
+
+        if n > 0 {
+            let mut t = 0;
+            for i in self.0.iter_mut().rev() {
+                let t2 = *i << (64 - n);
+                *i >>= n;
+                *i |= t;
+                t = t2;
+            }
+        }
+    }
 }
 
 impl From<Scalar> for [u8; 32] {
