765c96a69b736490e29d781673718017d06a8ad7
Julius-Syvis <55212122+Julius-Syvis@users.noreply.github.com>
2021-11-03 15:49:21 +0200

    add poly test

---
+++ from-scratch/benches/poly.rs
@@ -0,0 +1,10 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use kzg_bench::benches::poly::{bench_new_poly_div};
+use kzg_from_scratch::kzg_types::{FsFr, FsPoly};
+
+fn bench_new_poly_div_(c: &mut Criterion) {
+    bench_new_poly_div::<FsFr, FsPoly>(c);
+}
+
+criterion_group!(benches, bench_new_poly_div_);
+criterion_main!(benches);
--- kzg-bench/src/benches/poly.rs
+++ kzg-bench/src/benches/poly.rs
@@ -1,14 +1,37 @@
-// use criterion::{criterion_group, criterion_main, Criterion};
-// use kzg::Poly;
+use criterion::Criterion;
+use kzg::{Fr, Poly};
 
-// fn poly_division_in_finite_field(c: &mut Criterion) {
-//     c.bench_function(
-//         "poly_division_in_finite_field",
-//         |b| b.iter(|| {
-//             Poly::bench_divide_in_finite_field(8)
-//         })
-//     );
-// }
+pub fn bench_new_poly_div<TFr: Fr, TPoly: Poly<TFr>>(c: &mut Criterion) {
+    for scale in 6..16 {
+        let dividend_length = 1 << scale;
+        let divisor_length = dividend_length / 2;
 
-// criterion_group!(benches, poly_division_in_finite_field);
-// criterion_main!(benches);
+        let mut dividend = TPoly::new(dividend_length).unwrap();
+        let mut divisor = TPoly::new(divisor_length).unwrap();
+
+        // Randomize the polynomials' coefficients
+        for i in 0..dividend.len() {
+            dividend.set_coeff_at(i, &TFr::rand());
+        }
+        for i in 0..divisor.len() {
+            divisor.set_coeff_at(i, &TFr::rand());
+        }
+
+        // Ensure that the polynomials' orders correspond to their lengths
+        if dividend.get_coeff_at(dividend.len() - 1).is_zero() {
+            dividend.set_coeff_at(dividend.len() - 1, &TFr::one());
+        }
+        if divisor.get_coeff_at(divisor.len() - 1).is_zero() {
+            divisor.set_coeff_at(divisor.len() - 1, &TFr::one());
+        }
+
+        let id = format!("bench_new_poly_div scale: '{}'", scale);
+        c.bench_function(&id, |b| b.iter(|| {
+            let mut divided_poly = dividend.div(&mut divisor).unwrap();
+            divided_poly.destroy();
+        }));
+
+        dividend.destroy();
+        divisor.destroy();
+    }
+}
