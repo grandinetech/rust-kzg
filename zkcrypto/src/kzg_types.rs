5d04df9837eca551e1658a8869bff1043be4412c
dominykasKaminskas <91206504+dominykasKaminskas@users.noreply.github.com>
2021-11-11 13:42:58 +0200

    Update kzg_types.rs

--- zkcrypto/src/kzg_types.rs
+++ zkcrypto/src/kzg_types.rs
@@ -45,6 +45,8 @@ use crate::kzg_proofs::{
 	KZGSettings as LKZGSettings
 };
 
+use kzg::KZGSettings;
+
  pub const G1_GENERATOR: ZkG1Projective = ZkG1Projective {
     x: ZkFp::from_raw_unchecked([0x5cb3_8790_fd53_0c16, 0x7817_fc67_9976_fff5, 0x154f_95c7_143b_a1c1, 0xf0ae_6acd_f3d0_e747, 0xedce_6ecc_21db_f440, 0x1201_7741_9e0b_fb75,] ),
     y: ZkFp::from_raw_unchecked([0xbaac_93d5_0ce7_2271, 0x8c22_631a_7918_fd8e, 0xdd59_5f13_5707_25ce, 0x51ac_5829_5040_5194, 0x0e1c_8c3f_ad00_59c0, 0x0bbc_3efc_5008_a26a,] ),
