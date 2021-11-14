6f26707854fafe73b357a1e61a4b5269f07cfb9a
belijzajac <tautvydas749@gmail.com>
2021-11-14 15:50:17 +0200

    Implemented `fk_single` test

--- ckzg/tests/fk20_proofs.rs
+++ ckzg/tests/fk20_proofs.rs
@@ -11,6 +11,11 @@ mod tests {
     use ckzg::utils::reverse_bits_limited;
 
     #[test]
+    fn test_fk_single() {
+        fk_single::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings, KzgKZGSettings, KzgFK20SingleSettings>(&generate_trusted_setup, &reverse_bits_limited);
+    }
+
+    #[test]
     fn test_fk_single_strided() {
         fk_single_strided::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings, KzgKZGSettings, KzgFK20SingleSettings>(&generate_trusted_setup, &reverse_bits_limited);
     }
