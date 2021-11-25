8a9bffd9e26e123b612022f700b386e690cb541c
belijzajac <tautvydas749@gmail.com>
2021-11-26 00:53:44 +0200

    Adapt benchmark to `lib.rs` changes

--- kzg-bench/src/benches/zero_poly.rs
+++ kzg-bench/src/benches/zero_poly.rs
@@ -10,14 +10,13 @@ pub fn bench_zero_poly<
 ) {
     for scale in 5..16 {
         let fs = TFFTSettings::new(scale).unwrap();
-        let size = TFFTSettings::get_max_width(&fs);
-        let mut missing = vec![usize::default(); size];
-        for i in 0..size {
+        let mut missing = vec![usize::default(); fs.get_max_width()];
+        for i in 0..fs.get_max_width() {
             missing[i] = i;
         }
         let id = format!("bench_zero_poly scale: '{}'", scale);
         c.bench_function(&id, |b| {
-            b.iter(|| TFFTSettings::zero_poly_via_multiplication(&fs, size, &missing))
+            b.iter(|| fs.zero_poly_via_multiplication(fs.get_max_width(), &missing, fs.get_max_width() / 2))
         });
     }
 }
