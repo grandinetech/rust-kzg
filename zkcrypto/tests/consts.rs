1ba2f35d686f526317c1c9f02df6c1f8a546a68e
Kastuks <kkostass9@gmail.com>
2021-12-24 15:57:36 +0200

    added consts test

--- zkcrypto/tests/consts.rs
+++ zkcrypto/tests/consts.rs
@@ -4,7 +4,7 @@ pub mod tests {
     use zkcrypto::fftsettings::{ZkFFTSettings};
 	use zkcrypto::zkfr::blsScalar;
 	use zkcrypto::consts::{SCALE2_ROOT_OF_UNITY, expand_root_of_unity};
-	
+	// use zkcrypto::poly::Fr; // used for slices tests
 	
 #[test]
 fn roots_of_unity_out_of_bounds_fails_() {
@@ -26,5 +26,25 @@ fn expand_roots_is_plausible_() {
 fn new_fft_settings_is_plausible_() {
 	new_fft_settings_is_plausible::<blsScalar, ZkFFTSettings>();
 }
+
+#[test]
+fn roots_of_unity_is_the_expected_size_() {
+    roots_of_unity_is_the_expected_size(&SCALE2_ROOT_OF_UNITY);
+}
+
+// unneeded tests because these were made for and by mcl team to test with slices
+// #[test]
+// fn test_roots_of_unity_are_plausible_slice() {
+	// roots_of_unity_are_plausible_slice::<blsScalar>(&SCALE2_ROOT_OF_UNITY.iter()
+		// .map(blsScalar::from_u64_arr)
+		// .collect::<Vec<_>>());
+// }
+
+// #[test]
+// fn test_expand_roots_is_plausible_slice() {
+	// expand_roots_is_plausible_slice::<blsScalar>(&SCALE2_ROOT_OF_UNITY.iter()
+		// .map(blsScalar::from_u64_arr)
+		// .collect::<Vec<_>>(), &expand_root_of_unity);
+// }
 
 }
