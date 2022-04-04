3ebab9054cb7c1ebbfb70a757ded5dfcca7d435f
belijzajac <tautvydas749@gmail.com>
2022-04-04 21:31:09 +0300

    Setup mosh server

--- setup-env.sh
+++ setup-env.sh
@@ -3,5 +3,10 @@
 # 1.1. setup environment and then restart shell
 
 apt update
-apt -y install htop neofetch gcc g++ clang llvm libomp-dev make git
+apt -y install htop neofetch gcc g++ clang llvm libomp-dev make git mosh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# 1.1.1. setup mosh-server
+locale-gen "en_US.UTF-8"
+update-locale LC_ALL="en_US.UTF-8"
+echo LANG=en_US.UTF-8 > /etc/locale.conf
