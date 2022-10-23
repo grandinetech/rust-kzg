ac97b31eefa36a4ac4fcd76268f72208dd7d55be
Mdominykas <marmadominykas@gmail.com>
2022-10-23 16:19:16 +0300

    bytes_to_bls_field test

--- ckzg/tests/eip_4844.rs
+++ ckzg/tests/eip_4844.rs
@@ -4,9 +4,13 @@ mod tests {
 
     use ckzg::{eip_4844::{compute_powers, bytes_to_bls_field}, finite::BlstFr};
     use kzg_bench::tests::eip_4844::{
-        compute_powers_test
+        compute_powers_test, bytes_to_bls_field_test
     };
 
+    #[test]
+    pub fn bytes_to_bls_field_test_() {
+        bytes_to_bls_field_test::<BlstFr>(&bytes_to_bls_field);
+    }
 
     #[test]
     pub fn compute_powers_test_() {
