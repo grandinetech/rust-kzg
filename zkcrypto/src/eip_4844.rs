08a48f17780aaa1f1f6a7437ef7bd0325843726f
Mantas S <mantas487@gmail.com>
2022-10-29 10:32:30 +0300

    Update eip_4844.rs

--- zkcrypto/src/eip_4844.rs
+++ zkcrypto/src/eip_4844.rs
@@ -1,7 +1,7 @@
 use crate::zkfr::blsScalar;
 
 pub fn bytes_to_bls_field(bytes: &[u8; 32usize]) -> blsScalar {
-    blsScalar::from_bytes(&bytes).unwrap()
+    blsScalar::from_bytes(bytes).unwrap()
 }
 
 pub fn bytes_from_bls_field(fr: &blsScalar) -> [u8; 32usize] {
