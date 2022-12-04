1ad6144643e763fd807498502863000baaf6d49d
Mdominykas <marmadominykas@gmail.com>
2022-12-04 19:48:17 +0200

    added more tests for ckzg

--- ckzg/tests/eip_4844.rs
+++ ckzg/tests/eip_4844.rs
@@ -14,7 +14,7 @@ mod tests {
     };
     use kzg_bench::tests::eip_4844::{
         bytes_to_bls_field_test, compute_powers_test,
-        eip4844_test, blob_to_kzg_commitment_test,
+        eip4844_test, blob_to_kzg_commitment_test, compute_aggregate_kzg_proof_test_empty, aggregate_proof_for_single_blob_test,
     };
 
     #[test]
@@ -35,6 +35,25 @@ mod tests {
     #[test]
     pub fn blob_to_kzg_commitment_test_(){
         blob_to_kzg_commitment_test::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(&load_trusted_setup_rust, &blob_to_kzg_commitment_rust, &bytes_from_g1_rust);
+    }
+
+    #[test]
+    pub fn compute_aggregate_kzg_proof_test_empty_() {
+        compute_aggregate_kzg_proof_test_empty::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+            &load_trusted_setup_rust,
+            &compute_aggregate_kzg_proof_rust,
+            &bytes_from_g1_rust,
+        )
+    }
+
+    #[test]
+    pub fn aggregate_proof_for_single_blob_test_() {
+        aggregate_proof_for_single_blob_test::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+            &load_trusted_setup_rust,
+            &blob_to_kzg_commitment_rust,
+            &compute_aggregate_kzg_proof_rust,
+            &verify_aggregate_kzg_proof_rust,
+        );
     }
 
 }
