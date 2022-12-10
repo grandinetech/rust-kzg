b5dc477c0c1e4b4299d79a2d3645f42d43d82aa0
belijzajac <tautvydas749@gmail.com>
2022-12-10 19:55:45 +0200

    Add compute powers test

--- Arkworks/src/eip_4844.rs
+++ Arkworks/src/eip_4844.rs
@@ -1,3 +1,4 @@
+use kzg::Fr;
 use crate::kzg_types::FsFr;
 
 pub fn bytes_from_bls_field(fr: &FsFr) -> [u8; 32usize] {
@@ -6,4 +7,16 @@ pub fn bytes_from_bls_field(fr: &FsFr) -> [u8; 32usize] {
 
 pub fn bytes_to_bls_field(bytes: &[u8; 32usize]) -> FsFr {
     FsFr::from_scalar(*bytes)
+}
+
+pub fn compute_powers(base: &FsFr, num_powers: usize) -> Vec<FsFr> {
+    let mut powers: Vec<FsFr> = vec![FsFr::default(); num_powers];
+    if num_powers == 0 {
+        return powers;
+    }
+    powers[0] = FsFr::one();
+    for i in 1..num_powers {
+        powers[i] = powers[i - 1].mul(base);
+    }
+    powers
 }
--- Arkworks/tests/eip_4844.rs
+++ Arkworks/tests/eip_4844.rs
@@ -1,11 +1,16 @@
 #[cfg(test)]
 pub mod tests {
-    use arkworks::eip_4844::{bytes_from_bls_field, bytes_to_bls_field};
-    use kzg_bench::tests::eip_4844::{bytes_to_bls_field_test};
+    use arkworks::eip_4844::{bytes_from_bls_field, bytes_to_bls_field, compute_powers};
+    use kzg_bench::tests::eip_4844::{bytes_to_bls_field_test, compute_powers_test};
     use arkworks::kzg_types::FsFr;
 
     #[test]
     fn bytes_to_bls_field_test_() {
         bytes_to_bls_field_test::<FsFr>(&bytes_to_bls_field, &bytes_from_bls_field);
+    }
+
+    #[test]
+    pub fn compute_powers_test_() {
+        compute_powers_test::<FsFr>(&bytes_to_bls_field, &compute_powers);
     }
 }
