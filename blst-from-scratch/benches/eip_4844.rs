6d2ebecf3f4f3fe8ee0ef954cc53054407435e97
Kalpokas_Simonas <simonas.kalpokas@gmail.com>
2022-12-12 11:44:50 +0200

    Add parameterized benchmarks for verification

--- blst-from-scratch/benches/eip_4844.rs
+++ blst-from-scratch/benches/eip_4844.rs
@@ -13,23 +13,28 @@ use kzg_bench::benches::eip_4844::{
     bench_compute_aggregate_kzg_proof, bench_verify_aggregate_kzg_proof,
 };
 
+fn bench_compute_aggregate_kzg_proof_1_(c: &mut Criterion) {
+    bench_compute_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
+        c,
+        &load_trusted_setup,
+        &compute_aggregate_kzg_proof,
+        1,
+    )
+}
+
 fn bench_compute_aggregate_kzg_proof_2_(c: &mut Criterion) {
     bench_compute_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
         c,
         &load_trusted_setup,
         &compute_aggregate_kzg_proof,
-
         2,
     )
 }
-
-
 fn bench_compute_aggregate_kzg_proof_4_(c: &mut Criterion) {
     bench_compute_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
         c,
         &load_trusted_setup,
         &compute_aggregate_kzg_proof,
-
         4,
     )
 }
@@ -52,20 +57,65 @@ fn bench_compute_aggregate_kzg_proof_16_(c: &mut Criterion) {
     )
 }
 
-fn bench_verify_aggregate_kzg_proof_(c: &mut Criterion) {
+fn bench_verify_aggregate_kzg_proof_1_(c: &mut Criterion) {
     bench_verify_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
         c,
         &load_trusted_setup,
         &blob_to_kzg_commitment,
         &compute_aggregate_kzg_proof,
         &verify_aggregate_kzg_proof,
+        1,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_2_(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
+        c,
+        &load_trusted_setup,
+        &blob_to_kzg_commitment,
+        &compute_aggregate_kzg_proof,
+        &verify_aggregate_kzg_proof,
+        2,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_4_(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
+        c,
+        &load_trusted_setup,
+        &blob_to_kzg_commitment,
+        &compute_aggregate_kzg_proof,
+        &verify_aggregate_kzg_proof,
+        4,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_8_(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
+        c,
+        &load_trusted_setup,
+        &blob_to_kzg_commitment,
+        &compute_aggregate_kzg_proof,
+        &verify_aggregate_kzg_proof,
+        8,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_16_(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<FsFr, FsG1, FsG2, FsPoly, FsFFTSettings, FsKZGSettings>(
+        c,
+        &load_trusted_setup,
+        &blob_to_kzg_commitment,
+        &compute_aggregate_kzg_proof,
+        &verify_aggregate_kzg_proof,
+        16,
     )
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = bench_compute_aggregate_kzg_proof_2_, bench_compute_aggregate_kzg_proof_4_, bench_compute_aggregate_kzg_proof_8_, bench_compute_aggregate_kzg_proof_16_, bench_verify_aggregate_kzg_proof_
+    targets =
+        bench_compute_aggregate_kzg_proof_1_, bench_compute_aggregate_kzg_proof_2_,
+        bench_compute_aggregate_kzg_proof_4_, bench_compute_aggregate_kzg_proof_8_, bench_compute_aggregate_kzg_proof_16_,
+        bench_verify_aggregate_kzg_proof_1_, bench_verify_aggregate_kzg_proof_2_,
+        bench_verify_aggregate_kzg_proof_4_, bench_verify_aggregate_kzg_proof_8_, bench_verify_aggregate_kzg_proof_16_
 }
 
 criterion_main!(benches);
--- ckzg/benches/eip_4844.rs
+++ ckzg/benches/eip_4844.rs
@@ -11,6 +11,22 @@ use kzg_bench::benches::eip_4844::{
     bench_compute_aggregate_kzg_proof, bench_verify_aggregate_kzg_proof,
 };
 
+fn bench_compute_aggregate_kzg_proof_1(c: &mut Criterion) {
+    bench_compute_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+        c,
+        &load_trusted_setup_rust,
+        &compute_aggregate_kzg_proof_rust,
+        1,
+    )
+}
+fn bench_compute_aggregate_kzg_proof_2(c: &mut Criterion) {
+    bench_compute_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+        c,
+        &load_trusted_setup_rust,
+        &compute_aggregate_kzg_proof_rust,
+        2,
+    )
+}
 fn bench_compute_aggregate_kzg_proof_4(c: &mut Criterion) {
     bench_compute_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
         c,
@@ -38,20 +54,64 @@ fn bench_compute_aggregate_kzg_proof_16(c: &mut Criterion) {
     )
 }
 
-fn bench_verify_aggregate_kzg_proof_(c: &mut Criterion) {
+fn bench_verify_aggregate_kzg_proof_1(c: &mut Criterion) {
     bench_verify_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
         c,
         &load_trusted_setup_rust,
         &blob_to_kzg_commitment_rust,
         &compute_aggregate_kzg_proof_rust,
         &verify_aggregate_kzg_proof_rust,
+        1,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_2(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+        c,
+        &load_trusted_setup_rust,
+        &blob_to_kzg_commitment_rust,
+        &compute_aggregate_kzg_proof_rust,
+        &verify_aggregate_kzg_proof_rust,
+        2,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_4(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+        c,
+        &load_trusted_setup_rust,
+        &blob_to_kzg_commitment_rust,
+        &compute_aggregate_kzg_proof_rust,
+        &verify_aggregate_kzg_proof_rust,
+        4,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_8(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+        c,
+        &load_trusted_setup_rust,
+        &blob_to_kzg_commitment_rust,
+        &compute_aggregate_kzg_proof_rust,
+        &verify_aggregate_kzg_proof_rust,
+        8,
+    )
+}
+fn bench_verify_aggregate_kzg_proof_16(c: &mut Criterion) {
+    bench_verify_aggregate_kzg_proof::<BlstFr, BlstP1, BlstP2, KzgPoly, KzgFFTSettings4844, KzgKZGSettings4844>(
+        c,
+        &load_trusted_setup_rust,
+        &blob_to_kzg_commitment_rust,
+        &compute_aggregate_kzg_proof_rust,
+        &verify_aggregate_kzg_proof_rust,
+        16,
     )
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = bench_compute_aggregate_kzg_proof_4, bench_compute_aggregate_kzg_proof_8, bench_compute_aggregate_kzg_proof_16, bench_verify_aggregate_kzg_proof_
+    targets = bench_compute_aggregate_kzg_proof_1, bench_compute_aggregate_kzg_proof_2
+        bench_compute_aggregate_kzg_proof_4, bench_compute_aggregate_kzg_proof_8, bench_compute_aggregate_kzg_proof_16, 
+        bench_verify_aggregate_kzg_proof_1, bench_verify_aggregate_kzg_proof_2, 
+        bench_verify_aggregate_kzg_proof_4, bench_verify_aggregate_kzg_proof_8, bench_verify_aggregate_kzg_proof_16
 }
 
 criterion_main!(benches);
