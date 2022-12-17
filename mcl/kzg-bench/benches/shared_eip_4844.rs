73facdca5e57456b159d3ea7c06572242aef0ea4
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2022-12-17 18:48:02 +0200

    mcl: catch up with benchmarks

--- mcl/kzg-bench/benches/shared_eip_4844.rs
+++ mcl/kzg-bench/benches/shared_eip_4844.rs
@@ -6,12 +6,10 @@ use mcl_rust::eip_4844::*;
 use mcl_rust::mcl_methods::init;
 use mcl_rust::CurveType;
 
-/*
 fn compute_aggregate_kzg_proof_bench_1(c: &mut Criterion) {
     assert!(init(CurveType::BLS12_381));
     bench_compute_aggregate_kzg_proof(c, &load_trusted_setup, &compute_aggregate_kzg_proof, 1)
 }
-*/
 
 fn compute_aggregate_kzg_proof_bench_2(c: &mut Criterion) {
     assert!(init(CurveType::BLS12_381));
@@ -33,7 +31,6 @@ fn compute_aggregate_kzg_proof_bench_16(c: &mut Criterion) {
     bench_compute_aggregate_kzg_proof(c, &load_trusted_setup, &compute_aggregate_kzg_proof, 16)
 }
 
-/*
 fn verify_aggregate_kzg_proof_bench_1(c: &mut Criterion) {
     assert!(init(CurveType::BLS12_381));
     bench_verify_aggregate_kzg_proof(
@@ -45,7 +42,6 @@ fn verify_aggregate_kzg_proof_bench_1(c: &mut Criterion) {
         1,
     )
 }
-*/
 
 fn verify_aggregate_kzg_proof_bench_2(c: &mut Criterion) {
     assert!(init(CurveType::BLS12_381));
@@ -100,7 +96,7 @@ fn verify_aggregate_kzg_proof_bench_16(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = compute_aggregate_kzg_proof_bench_2, compute_aggregate_kzg_proof_bench_4, compute_aggregate_kzg_proof_bench_8, compute_aggregate_kzg_proof_bench_16, verify_aggregate_kzg_proof_bench_2, verify_aggregate_kzg_proof_bench_4, verify_aggregate_kzg_proof_bench_8, verify_aggregate_kzg_proof_bench_16
+    targets = compute_aggregate_kzg_proof_bench_1, compute_aggregate_kzg_proof_bench_2, compute_aggregate_kzg_proof_bench_4, compute_aggregate_kzg_proof_bench_8, compute_aggregate_kzg_proof_bench_16, verify_aggregate_kzg_proof_bench_1, verify_aggregate_kzg_proof_bench_2, verify_aggregate_kzg_proof_bench_4, verify_aggregate_kzg_proof_bench_8, verify_aggregate_kzg_proof_bench_16
 }
 
 criterion_main!(benches);
