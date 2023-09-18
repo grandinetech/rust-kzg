7858693d2f3bd646ece78dbfd52de4d16cb489e8
povilassl <slivinskas.povilas@gmail.com>
2023-09-18 19:35:49 +0300

    updating Cargo.toml files, Cargo.lock and changing rust-blst-4844 version

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -54,7 +54,7 @@ cargo rustc --release --crate-type=staticlib --features=parallel
 mv ../target/release/librust_kzg_blst.a ../target/release/rust_kzg_blst.a
 git clone https://github.com/ethereum/c-kzg-4844.git
 cd c-kzg-4844 || exit
-git -c advice.detachedHead=false checkout 5703f6f3536b7692616bc289ac3f3867ab8db9d8 # TODO: keep this updated
+git -c advice.detachedHead=false checkout fbef59a3f9e8fa998bdb5069d212daf83d586aa5 # TODO: keep this updated
 git submodule update --init
 cd src || exit
 export CFLAGS="-Ofast -fno-builtin-memcpy -fPIC -Wall -Wextra -Werror"
