56c3ad0f3b79fd2bcb507814dfca2bb03b9dd658
Justuxs <pegasas@yahoo.com>
2023-09-20 12:35:01 +0300

    Fix try -7

--- zkcrypto/src/curve/scalar.rs
+++ zkcrypto/src/curve/scalar.rs
@@ -885,7 +885,19 @@ impl Product for Scalar {
         result
     }
 }
+impl<'a> Product<&'a Scalar> for &'a Scalar {
+    fn product<I: Iterator<Item=&'a Scalar>>(iter: I) -> Self {
+        let mut result = Scalar([1, 1, 1, 1]);
 
+        for element in iter {
+            for i in 0..4 {
+                result.0[i] *= element.0[i];
+            }
+        }
+
+        &result
+    }
+}
 impl Field for Scalar {
     fn random(mut rng: impl RngCore) -> Self {
         let mut buf = [0; 64];
