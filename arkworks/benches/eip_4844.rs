2d8c31a382a7d6dc206c7de7d31d77a274b25f8f
Domas Kalinauskas <domaskal@gmail.com>
2023-10-11 22:26:15 +0300

    fix bench compile error

--- arkworks/benches/eip_4844.rs
+++ arkworks/benches/eip_4844.rs
@@ -2,9 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use kzg::{blob_to_kzg_commitment_rust, compute_kzg_proof_rust, verify_blob_kzg_proof_batch_rust, compute_blob_kzg_proof_rust};
 use kzg::common_utils::{bytes_to_blob, verify_blob_kzg_proof_rust, verify_kzg_proof_rust};
 use kzg_bench::benches::eip_4844::bench_eip_4844;
-use rust_kzg_arkworks::eip_4844::{
-    load_trusted_setup
-};
+use rust_kzg_arkworks::eip_4844::load_trusted_setup_filename_rust;
 use rust_kzg_arkworks::kzg_proofs::{FFTSettings, KZGSettings};
 use rust_kzg_arkworks::kzg_types::{ArkG1, ArkG2, ArkFr};
 use rust_kzg_arkworks::utils::PolyData;
@@ -12,7 +10,7 @@ use rust_kzg_arkworks::utils::PolyData;
 fn bench_eip_4844_(c: &mut Criterion) {
     bench_eip_4844::<ArkFr, ArkG1, ArkG2, PolyData, FFTSettings, KZGSettings>(
         c,
-        &load_trusted_setup,
+        &load_trusted_setup_filename_rust,
         &blob_to_kzg_commitment_rust,
         &bytes_to_blob,
         &compute_kzg_proof_rust,
