0c018fb569ceaf40e550aa4d3cf4ec71557abdc6
sirse <artiom.tretjakovas2@gmail.com>
2024-06-05 21:24:11 +0300

    Update benchmark script

--- run-cuda-benchmarks.sh
+++ run-cuda-benchmarks.sh
@@ -30,6 +30,10 @@ SECONDS=0
 paste_name="linode_benchmarks"
 paste_file="$paste_name.txt"
 
+print_cores_msg () {
+  printf "\n\n\n********** %s **********\n\n\n" "$1" >> "$2"
+}
+
 print_msg () {
   printf "\n\n\n~~~~~~~~~~ %s ~~~~~~~~~~\n\n\n" "$1" >> "$2"
 }
@@ -44,34 +48,55 @@ git clone https://github.com/ArtiomTr/rust-kzg.git
 cd rust-kzg || exit
 git checkout Integrate_sppark_msm || exit
 
+print_cores_msg "BENCH EIP"
+
 # 3. run benchmarks
 print_msg "rust-kzg with blst backend (parallel, bgmw)" ../"$paste_file"
 cargo bench --manifest-path blst/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
-cargo bench --manifest-path blst/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
 
 print_msg "rust-kzg with blst backend (sppark)" ../"$paste_file"
 cargo bench --manifest-path blst/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel,sppark >> ../"$paste_file"
-cargo bench --manifest-path blst/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,sppark >> ../"$paste_file"
 
 print_msg "rust-kzg with arkworks backend (parallel, bgmw)" ../"$paste_file"
 cargo bench --manifest-path arkworks/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
-cargo bench --manifest-path arkworks/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,bgmw 
 
-print_msg "rust-kzg with arkworks3 backend (parallel, bgmw)" ../"$paste_file"
+print_msg "rust-kzg with arkworks3 backend (parallel)" ../"$paste_file"
 cargo bench --manifest-path arkworks3/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel >> ../"$paste_file"
-cargo bench --manifest-path arkworks3/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel >> ../"$paste_file"
 
 print_msg "rust-kzg with arkworks3 backend (sppark)" ../"$paste_file"
 cargo bench --manifest-path arkworks3/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel,sppark >> ../"$paste_file"
-cargo bench --manifest-path arkworks3/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,sppark >> ../"$paste_file"
 
 print_msg "rust-kzg with arkworks3 backend (sppark_wlc)" ../"$paste_file"
 cargo bench --manifest-path arkworks3/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel,sppark_wlc >> ../"$paste_file"
-cargo bench --manifest-path arkworks3/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,sppark_wlc >> ../"$paste_file"
 
-print_msg "rust-kzg with constantine backend (parallel, bgmw)" ../"$paste_file"
-cargo bench --manifest-path constantine/Cargo.toml --bench eip_4844 --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
-cargo bench --manifest-path constantine/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
+point_count=("12" "14" "16" "18" "20")
+jobs_count=${#point_count[@]}
+
+for (( i=0; i<jobs_count; i++ ));
+do
+  print_cores_msg "BENCHMARKING 2^${point_count[$i]} POINTS" "$paste_file"
+  
+  export BENCH_NPOW=point_count[$i]
+
+  print_msg "rust-kzg with blst backend (parallel, bgmw)" ../"$paste_file"
+  cargo bench --manifest-path blst/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
+
+  print_msg "rust-kzg with blst backend (sppark)" ../"$paste_file"
+  cargo bench --manifest-path blst/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,sppark >> ../"$paste_file"
+
+  print_msg "rust-kzg with arkworks backend (parallel, bgmw)" ../"$paste_file"
+  cargo bench --manifest-path arkworks/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,bgmw >> ../"$paste_file"
+
+  print_msg "rust-kzg with arkworks3 backend (parallel)" ../"$paste_file"
+  cargo bench --manifest-path arkworks3/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel >> ../"$paste_file"
+
+  print_msg "rust-kzg with arkworks3 backend (sppark)" ../"$paste_file"
+  cargo bench --manifest-path arkworks3/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,sppark >> ../"$paste_file"
+
+  print_msg "rust-kzg with arkworks3 backend (sppark_wlc)" ../"$paste_file"
+  cargo bench --manifest-path arkworks3/Cargo.toml --bench lincomb --no-default-features --features std,rand,parallel,sppark_wlc >> ../"$paste_file"
+
+done
 
 cd ..
 
