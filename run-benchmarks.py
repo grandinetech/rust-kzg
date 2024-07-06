297aa6411068b606000c8423af2a951e16d6769d
sirse <artiom.tretjakovas2@gmail.com>
2024-07-06 15:25:54 +0300

    Log stderr in python script

--- run-benchmarks.py
+++ run-benchmarks.py
@@ -30,7 +30,7 @@ async def main():
         ),
     ) as bot:
         await bot.send_message(chat_id=chat_id, text="Script started")
-    output = subprocess.run("./run-benchmarks.sh > logs.txt", shell=True)
+    output = subprocess.run("./run-benchmarks.sh > logs.txt 2>&1", shell=True)
     async with Bot(
         token=token,
         default=DefaultBotProperties(
