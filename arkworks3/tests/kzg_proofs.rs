144f95ffc726cc77679e3e906de80c54957a7f15
Andenis <andenisbu@gmail.com>
2024-10-23 00:26:23 +0300

    Fix all problem messages

--- arkworks3/tests/kzg_proofs.rs
+++ arkworks3/tests/kzg_proofs.rs
@@ -7,12 +7,15 @@ mod tests {
     use rust_kzg_arkworks3::kzg_types::{ArkFp, ArkFr, ArkG1, ArkG1Affine, ArkG2};
     use rust_kzg_arkworks3::utils::PolyData;
 
+    #[ignore = "KZG settings loading doesn't support trusted setup sizes other than FIELD_ELEMENTS_PER_BLOB (4096 points)"]
     #[test]
     fn proof_single_() {
         proof_single::<ArkFr, ArkG1, ArkG2, PolyData, FFTSettings, KZGSettings, ArkFp, ArkG1Affine>(
             &generate_trusted_setup,
         );
     }
+
+    #[ignore = "KZG settings loading doesn't support trusted setup sizes other than FIELD_ELEMENTS_PER_BLOB (4096 points)"]
     #[test]
     fn commit_to_nil_poly_() {
         commit_to_nil_poly::<
@@ -26,6 +29,8 @@ mod tests {
             ArkG1Affine,
         >(&generate_trusted_setup);
     }
+
+    #[ignore = "KZG settings loading doesn't support trusted setup sizes other than FIELD_ELEMENTS_PER_BLOB (4096 points)"]
     #[test]
     fn commit_to_too_long_poly_() {
         commit_to_too_long_poly_returns_err::<
@@ -40,6 +45,7 @@ mod tests {
         >(&generate_trusted_setup);
     }
 
+    #[ignore = "KZG settings loading doesn't support trusted setup sizes other than FIELD_ELEMENTS_PER_BLOB (4096 points)"]
     #[test]
     fn proof_multi_() {
         proof_multi::<ArkFr, ArkG1, ArkG2, PolyData, FFTSettings, KZGSettings, ArkFp, ArkG1Affine>(
