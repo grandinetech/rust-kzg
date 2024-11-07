b2f42933f44c0b42a7b5d4d7157a9c3ff59bfe83
Vilius <vilius.bakutis23@gmail.com>
2024-11-07 13:47:08 +0200

    fix

--- arkworks/tests/fft_g1.rs
+++ arkworks/tests/fft_g1.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use kzg_bench::tests::fft_g1::{compare_sft_fft, roundtrip_fft, stride_fft};
+    use kzg_bench::tests::fft_g1::{roundtrip_fft, stride_fft};
     use rust_kzg_arkworks::fft_g1::{fft_g1_fast, fft_g1_slow, make_data};
     use rust_kzg_arkworks::kzg_proofs::FFTSettings;
     use rust_kzg_arkworks::kzg_types::{ArkFr, ArkG1};
@@ -17,6 +17,6 @@ mod tests {
 
     #[test]
     fn compare_sft_fft_() {
-        compare_sft_fft::<ArkFr, ArkG1, FFTSettings>(&fft_g1_slow, &fft_g1_fast, &make_data);
+        compare_ft_fft::<ArkFr, ArkG1, FFTSettings>(&fft_g1_slow, &fft_g1_fast, &make_data);
     }
 }
