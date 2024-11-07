6ae62f3a27af56b8f7d957c4544433828e991cea
Vilius <vilius.bakutis23@gmail.com>
2024-11-07 13:34:53 +0200

    make tests pass

--- arkworks/tests/eip_7594.rs
+++ arkworks/tests/eip_7594.rs
@@ -1,4 +1,3 @@
-
 #[cfg(test)]
 mod tests {
     use kzg::eip_4844::bytes_to_blob;
@@ -6,17 +5,27 @@ mod tests {
         test_vectors_compute_cells_and_kzg_proofs, test_vectors_recover_cells_and_kzg_proofs,
         test_vectors_verify_cell_kzg_proof_batch,
     };
-    use rust_kzg_constantine::{
-        eip_4844::load_trusted_setup_filename_rust,
-        eip_7594::{
-            compute_cells_and_kzg_proofs_rust, recover_cells_and_kzg_proofs_rust,
-            verify_cell_kzg_proof_batch_rust,
-        },
+    use rust_kzg_arkworks::eip_4844::load_trusted_setup_filename_rust;
+    use rust_kzg_arkworks::eip_7594::{
+        compute_cells_and_kzg_proofs_rust, recover_cells_and_kzg_proofs_rust,
+        verify_cell_kzg_proof_batch_rust,
     };
+    use rust_kzg_arkworks::kzg_proofs::{LFFTSettings, LKZGSettings};
+    use rust_kzg_arkworks::kzg_types::{ArkFp, ArkFr, ArkG1, ArkG1Affine, ArkG2};
+    use rust_kzg_arkworks::utils::PolyData;
 
     #[test]
     pub fn test_vectors_compute_cells_and_kzg_proofs_() {
-        test_vectors_compute_cells_and_kzg_proofs(
+        test_vectors_compute_cells_and_kzg_proofs::<
+            ArkFr,
+            ArkG1,
+            ArkG2,
+            PolyData,
+            LFFTSettings,
+            LKZGSettings,
+            ArkFp,
+            ArkG1Affine,
+        >(
             &load_trusted_setup_filename_rust,
             &compute_cells_and_kzg_proofs_rust,
             &bytes_to_blob,
