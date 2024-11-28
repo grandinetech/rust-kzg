f01d362a2d1d1b409abfb2068160c6777c71732a
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-11-28 16:00:17 +0200

    MCL fp implementation

--- mcl/src/types/fp.rs
+++ mcl/src/types/fp.rs
@@ -1,13 +1,17 @@
 use blst::blst_fp;
 use kzg::G1Fp;
 
+use crate::mcl_methods::{mclBnFp_add, mclBnFp_inv, mclBnFp_mul, mclBnFp_neg, mclBnFp_sqr, mclBnFp_sub, try_init_mcl, MFp};
+
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy, Eq, PartialEq)]
-pub struct FsFp(pub blst_fp);
+pub struct FsFp(pub MFp);
 impl G1Fp for FsFp {
     fn one() -> Self {
-        Self(blst_fp {
-            l: [
+        try_init_mcl();
+            
+        Self(MFp {
+            d: [
                 8505329371266088957,
                 17002214543764226050,
                 6865905132761471162,
@@ -17,14 +21,20 @@ impl G1Fp for FsFp {
             ],
         })
     }
+
     fn zero() -> Self {
-        Self(blst_fp {
-            l: [0, 0, 0, 0, 0, 0],
+        try_init_mcl();
+
+        Self(MFp {
+            d: [0, 0, 0, 0, 0, 0],
         })
     }
+
     fn bls12_381_rx_p() -> Self {
-        Self(blst_fp {
-            l: [
+        try_init_mcl();
+
+        Self(MFp {
+            d: [
                 8505329371266088957,
                 17002214543764226050,
                 6865905132761471162,
@@ -36,54 +46,61 @@ impl G1Fp for FsFp {
     }
 
     fn inverse(&self) -> Option<Self> {
+        try_init_mcl();
+
         let mut out: Self = *self;
-        unsafe {
-            blst::blst_fp_inverse(&mut out.0, &self.0);
-        }
+        MFp::inv(&mut out.0, &self.0);
+
         Some(out)
     }
 
     fn square(&self) -> Self {
-        let mut out: Self = Default::default();
-        unsafe {
-            blst::blst_fp_sqr(&mut out.0, &self.0);
-        }
+        try_init_mcl();
+
+        let mut out: Self = *self;
+        MFp::sqr(&mut out.0, &self.0);
+
         out
     }
 
     fn double(&self) -> Self {
+        try_init_mcl();
+        
         let mut out: Self = Default::default();
+
         unsafe {
-            blst::blst_fp_add(&mut out.0, &self.0, &self.0);
+            mclBnFp_add(&mut out.0, &self.0, &self.0);
         }
+
         out
     }
 
     fn from_underlying_arr(arr: &[u64; 6]) -> Self {
-        Self(blst_fp { l: *arr })
+        try_init_mcl();
+
+        Self(MFp { d: *arr })
     }
 
     fn neg_assign(&mut self) {
+        try_init_mcl();
+        
         unsafe {
-            blst::blst_fp_cneg(&mut self.0, &self.0, true);
+            mclBnFp_neg(&mut self.0, &self.0);
         }
     }
 
     fn mul_assign_fp(&mut self, b: &Self) {
-        unsafe {
-            blst::blst_fp_mul(&mut self.0, &self.0, &b.0);
-        }
+        try_init_mcl();
+        self.0 *= &b.0;
     }
 
     fn sub_assign_fp(&mut self, b: &Self) {
-        unsafe {
-            blst::blst_fp_sub(&mut self.0, &self.0, &b.0);
-        }
+        try_init_mcl();
+        self.0 -= &b.0;
     }
 
     fn add_assign_fp(&mut self, b: &Self) {
-        unsafe {
-            blst::blst_fp_add(&mut self.0, &self.0, &b.0);
-        }
+        try_init_mcl();
+        self.0 += &b.0;
     }
 }
