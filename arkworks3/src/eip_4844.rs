38055394d94994ae33c0779789be84d409276f01
sirse <artiom.tretjakovas2@gmail.com>
2024-12-28 13:52:29 +0200

    Fix java tests for arkworks & arkworks3 backends

--- arkworks3/src/eip_4844.rs
+++ arkworks3/src/eip_4844.rs
@@ -76,6 +76,19 @@ pub unsafe extern "C" fn load_trusted_setup(
     num_g2_monomial_bytes: u64,
     _precompute: u64,
 ) -> CKzgRet {
+    *out = CKZGSettings {
+        brp_roots_of_unity: ptr::null_mut(),
+        roots_of_unity: ptr::null_mut(),
+        reverse_roots_of_unity: ptr::null_mut(),
+        g1_values_monomial: ptr::null_mut(),
+        g1_values_lagrange_brp: ptr::null_mut(),
+        g2_values_monomial: ptr::null_mut(),
+        x_ext_fft_columns: ptr::null_mut(),
+        tables: ptr::null_mut(),
+        wbits: 0,
+        scratch_size: 0,
+    };
+
     let g1_monomial_bytes =
         core::slice::from_raw_parts(g1_monomial_bytes, num_g1_monomial_bytes as usize);
     let g1_lagrange_bytes =
@@ -104,6 +117,19 @@ pub unsafe extern "C" fn load_trusted_setup_file(
     out: *mut CKZGSettings,
     in_: *mut FILE,
 ) -> CKzgRet {
+    *out = CKZGSettings {
+        brp_roots_of_unity: ptr::null_mut(),
+        roots_of_unity: ptr::null_mut(),
+        reverse_roots_of_unity: ptr::null_mut(),
+        g1_values_monomial: ptr::null_mut(),
+        g1_values_lagrange_brp: ptr::null_mut(),
+        g2_values_monomial: ptr::null_mut(),
+        x_ext_fft_columns: ptr::null_mut(),
+        tables: ptr::null_mut(),
+        wbits: 0,
+        scratch_size: 0,
+    };
+
     let mut buf = vec![0u8; 1024 * 1024];
     let len: usize = libc::fread(buf.as_mut_ptr() as *mut libc::c_void, 1, buf.len(), in_);
     let s = handle_ckzg_badargs!(String::from_utf8(buf[..len].to_vec()));
