f5bce465089d1ba389ebcedeaa87e8a99c36beb2
Vilius <vilius.bakutis23@gmail.com>
2025-01-02 11:20:26 +0200

    PeerDAS (EIP-7594) implementation for arkworks backend
    arkworks implementation eip-7594
    
    fix some errors
    
    fix more errors
    
    fix remaining ones
    
    fix
    
    remove
    
    remove unused imports
    
    try foix
    
    make tests pass
    
    fix
    
    fix again
    
    yet another fix
    
    fix test

--- arkworks/tests/kzg_proofs.rs
+++ arkworks/tests/kzg_proofs.rs
@@ -3,47 +3,26 @@ mod tests {
     use kzg_bench::tests::kzg_proofs::{
         commit_to_nil_poly, commit_to_too_long_poly_returns_err, proof_multi, proof_single,
     };
-    use rust_kzg_arkworks::kzg_proofs::{generate_trusted_setup, FFTSettings, KZGSettings};
-    use rust_kzg_arkworks::kzg_types::{ArkFp, ArkFr, ArkG1, ArkG1Affine, ArkG2};
-    use rust_kzg_arkworks::utils::PolyData;
+    use rust_kzg_arkworks::eip_7594::ArkBackend;
+    use rust_kzg_arkworks::kzg_proofs::generate_trusted_setup;
 
     #[test]
     fn proof_single_() {
-        proof_single::<ArkFr, ArkG1, ArkG2, PolyData, FFTSettings, KZGSettings, ArkFp, ArkG1Affine>(
-            &generate_trusted_setup,
-        );
+        proof_single::<ArkBackend>(&generate_trusted_setup);
     }
+
     #[test]
     fn commit_to_nil_poly_() {
-        commit_to_nil_poly::<
-            ArkFr,
-            ArkG1,
-            ArkG2,
-            PolyData,
-            FFTSettings,
-            KZGSettings,
-            ArkFp,
-            ArkG1Affine,
-        >(&generate_trusted_setup);
+        commit_to_nil_poly::<ArkBackend>(&generate_trusted_setup);
     }
+
     #[test]
     fn commit_to_too_long_poly_() {
-        commit_to_too_long_poly_returns_err::<
-            ArkFr,
-            ArkG1,
-            ArkG2,
-            PolyData,
-            FFTSettings,
-            KZGSettings,
-            ArkFp,
-            ArkG1Affine,
-        >(&generate_trusted_setup);
+        commit_to_too_long_poly_returns_err::<ArkBackend>(&generate_trusted_setup);
     }
 
     #[test]
     fn proof_multi_() {
-        proof_multi::<ArkFr, ArkG1, ArkG2, PolyData, FFTSettings, KZGSettings, ArkFp, ArkG1Affine>(
-            &generate_trusted_setup,
-        );
+        proof_multi::<ArkBackend>(&generate_trusted_setup);
     }
 }
